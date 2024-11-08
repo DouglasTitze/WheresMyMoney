6de552d9c4b4e1d2268723cb00a438f58c64569f
Douglas Titze <douglasmarcost2@gmail.com>
2024-11-08 16:10:22 -0500

    login and logout dont work

--- wheresmymoney/home/urls.py
+++ wheresmymoney/home/urls.py
@@ -4,9 +4,14 @@ from . import views
 
 urlpatterns = [
     # path('home/', views.home),
-    path('home/', views.HomeView.as_view()),
+    path('home/', views.HomeView.as_view(), name="home"),
     
     # path('authorized/', views.authorized)
-    path('authorized/', views.AuthorizedView.as_view()),
+    path('authorized/', views.AuthorizedView.as_view(), name="auth"),
+    
+    path('login/', views.LoginInterfaceView.as_view(), name="login"),
+    path('loggedout/', views.LogoutConfirmView.as_view(),name='loggedout'),
+
+    path('logout/', views.LogoutInterfaceView.as_view(),name='logout'),
     
 ]
--- wheresmymoney/home/views.py
+++ wheresmymoney/home/views.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
 
 
 # def home(request):
@@ -21,3 +22,12 @@ class HomeView(TemplateView):
 class AuthorizedView(LoginRequiredMixin, TemplateView):
     template_name = 'home/authorized.html'
     login_url = '/admin'
+
+class LoginInterfaceView(LoginView):
+    template_name = 'home/login.html'
+
+class LogoutInterfaceView(LogoutView):
+    template_name = 'home/loggedout.html'
+
+class LogoutConfirmView(TemplateView):
+    template_name = 'home/logout.html'
